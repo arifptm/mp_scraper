fac87a90bf898120cb3cc2ff5f88d9a5757aeca3
arifptm <arifptm@gmail.com>
2017-07-23 12:59:52 +0700

    asd
    asd

--- app/Http/Controllers/LinkCheckerController.php
+++ app/Http/Controllers/LinkCheckerController.php
@@ -4,44 +4,74 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
-use Goutte;
 use App\Marketplace;
 use App\Feed;
 
 class LinkCheckerController extends Controller
 {
     
-	public function checker($link)
+	public function checker($nodes)
 	{
+        $curl_arr = array();
+        $master = curl_multi_init();
 
-		Goutte::request('GET', $link);
-		if( Goutte::getResponse()->getStatus() == 404 )
-		{
-			$so = Item::update(['soldout'=>1]);
-		}
-		
+        foreach ($nodes as $i=>$url)
+        {
+            $curl_arr[$i] = curl_init($url);
+            curl_setopt($curl_arr[$i], CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl_arr[$i], CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl_arr[$i], CURLOPT_NOBODY, true);
+            curl_multi_add_handle($master, $curl_arr[$i]);
+        }
+
+        do {
+            curl_multi_exec($master,$running);
+        } while($running > 0);        
+        
+        foreach($nodes as $i=>$url)
+        {
+            $results[$url] = curl_getinfo  ( $curl_arr[$i], CURLINFO_HTTP_CODE  );
+        }        
+        
+        return $results;
 	}
+
 
     public function run($slug)
     {
-        $k = Marketplace::with('feed.item')->whereSlug($slug)->get();
-        //dd($k);
-
-        foreach ($k as $t){
-        	foreach ($t->feed as $u){
-        		foreach ($u->item as $key => $w) {
-        			$x[$key] = $this->checker($w->item_url);
-        			if ($key == 1 )
-        			{
-        				break;
-        			}
-        		}
-        	}
+        $mk = Marketplace::with(['feed.item' => function($query){
+            $query->where('checked', '=', 0)->where('title','!=', null )->where('sold_out','!=','1');
+        }])->whereSlug($slug)->first();
+        foreach($mk->feed as $feed){
+            foreach ($feed->item as $item) {
+                $i[$item->category_id] = $item->item_url;
+            }
         }
-        dd($x);
-
-        //$this->checker($l->item_url);
         
-        //return view ('admin.item.index', ['items'=> $p]);
+        dd()
+
+        $i = array_slice($i, 0, 20);
+        
+        foreach($this->checker($i) as $key=>$status){
+            $so = Item::whereItem_url($key)->first();
+            
+            if($status == 404){                
+                $so->update(['sold_out'=> 1, 'checked' => 1]);
+                echo $status." | ".$so->id." | ".$so->item_url."<br>";
+            } else {
+                $so->update(['checked' => 1]);
+                echo $status." | ".$so->id." | ".$so->item_url."<br>";
+            }
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
